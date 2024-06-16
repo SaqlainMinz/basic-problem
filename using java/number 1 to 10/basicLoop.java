01c326ede86bb84b53d934a2a83c4192ad2f72bf
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-16 22:19:50 +0800

    22 july

---
+++ using java/number 1 to 10/basicLoop.java
@@ -0,0 +1,11 @@
+public class basicLoop {
+  public static void main(String[] args) {
+    //print number 1 to 100
+    int initial=0;
+    while(initial<=10){
+      System.out.println(initial);
+      initial++;
+    }
+  }
+  
+}
