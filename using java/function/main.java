80bec0c755838cdf0c91e9731e40c7f463c6ca4b
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-21 16:00:08 +0530

    new update

---
+++ using java/function/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void printHelloWorld(){
+        System.out.println("hello World");
+    }
+    public static void main(String args[]){
+        printHelloWorld();
+        
+    }
+}
