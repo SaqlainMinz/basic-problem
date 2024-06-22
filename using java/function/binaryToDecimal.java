cf0733b5eb669ea1c9bf9153a671d681544b82d8
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-22 13:13:40 +0530

    22 june 24

--- using java/function/binaryToDecimal.java
+++ using java/function/binaryToDecimal.java
@@ -1,20 +1,20 @@
 public class binaryToDecimal {
-    public static void binToDec(int binnum){
-        int myNum=binnum;
-        int pow=0;
-        int dec=0;
-        while(binnum>0){
-            int lastDigit=binnum%10;
-            dec=dec+(lastDigit*(int)Math.pow(2,pow));
+    public static void binToDec(int binnum) {
+        int myNum = binnum;
+        int pow = 0;
+        int dec = 0;
+        while (binnum > 0) {
+            int lastDigit = binnum % 10;
+            dec = dec + (lastDigit * (int) Math.pow(2, pow));
             pow++;
-            binnum=binnum/10;
+            binnum = binnum / 10;
         }
-        System.out.println("DECIMAL NUMBER IS: "+dec);
+        System.out.println("DECIMAL OF " + myNum + " NUMBER IS: " + dec);
     }
+
     public static void main(String[] args) {
         binToDec(1011);
 
-        
     }
-    
+
 }
--- using java/function/decimalToBinary.java
+++ using java/function/decimalToBinary.java
@@ -1,18 +1,19 @@
 public class decimalToBinary {
-    public static void decTobin(int n){
-        int myNUm=n;
-        int pow=0;
-        int bin=0;
-        while(n>0){
-            int rem=n%2;
-            bin=bin+(rem*(int)Math.pow(10, pow));
+    public static void decTobin(int n) {
+        int myNUm = n;
+        int pow = 0;
+        int bin = 0;
+        while (n > 0) {
+            int rem = n % 2;
+            bin = bin + (rem * (int) Math.pow(10, pow));
             pow++;
-            n/=2;
+            n /= 2;
         }
-        System.out.println("binary number of "+myNUm+" is "+bin);
+        System.out.println("binary number of " + myNUm + " is " + bin);
     }
+
     public static void main(String[] args) {
         decTobin(3);
-        
+
     }
 }
