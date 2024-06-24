245d4c99afbb8e03fc36d0800c9411b45d8cadc3
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-24 22:06:44 +0530

    24 june 24

---
+++ using java/patternPart2/pattern.java
@@ -0,0 +1,21 @@
+public class pattern {
+    // Hollow Rectangle
+    public static void hollow_rectangle(int totRow, int totCol) {
+        for (int i = 1; i <= totCol; i++) {
+            for (int j = 1; j <= totRow; j++) {
+                if (i == 1 || j == 1 || i == totCol || j == totRow) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        hollow_rectangle(5, 4);
+
+    }
+}
