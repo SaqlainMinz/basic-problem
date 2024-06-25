d41f4133fb2a6be2e0e97bda4f179167314841d8
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-25 23:57:08 +0530

    25 june 24

--- using java/patternPart2/pattern.java
+++ using java/patternPart2/pattern.java
@@ -13,10 +13,23 @@ public class pattern {
         }
 
     }
-    //Inverted and rotated half pyramid
+
+    // Inverted and rotated half pyramid
+    public static void inverted_halfPyramind(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("   ");
+
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.err.println();
+        }
+
+    }
 
     public static void main(String[] args) {
-        hollow_rectangle(5, 4);
-
+inverted_halfPyramind(5);
     }
 }
