6538f8d7443d3f8e48317e2d1b2c43a27d3d4d81
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-26 12:53:18 +0530

    26 june 24

--- using java/patternPart2/pattern.java
+++ using java/patternPart2/pattern.java
@@ -40,7 +40,22 @@ public class pattern {
         }
     }
 
+    // Floyd's Triangle
+    public static void floyds(int n) {
+        int sum=1;
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                
+                System.out.print(sum+ " ");
+                sum++;
+            }
+            
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        num_invertedHalfPyramid(5);
+        floyds(5);
     }
 }
