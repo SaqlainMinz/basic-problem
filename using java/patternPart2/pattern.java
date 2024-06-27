822156d33f36a94258155ef952bdf52a750278ae
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-06-27 21:39:38 +0530

    27 june 24

--- using java/patternPart2/pattern.java
+++ using java/patternPart2/pattern.java
@@ -42,20 +42,55 @@ public class pattern {
 
     // Floyd's Triangle
     public static void floyds(int n) {
-        int sum=1;
+        int sum = 1;
         for (int i = 1; i <= n; i++) {
-            
+
             for (int j = 1; j <= i; j++) {
-                
-                System.out.print(sum+ " ");
+
+                System.out.print(sum + " ");
                 sum++;
             }
-            
+
+            System.out.println();
+        }
+    }
+
+    // Butterfly Pattern
+    public static void butterfly(int num) {
+        // first half
+        for (int i = 1; i <= num; i++) {
+            // star
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            // space
+            for (int j = 1; j <= 2 * (num - i); j++) {
+                System.out.print("   ");
+            }
+            // star
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (int i = num; i >= 1; i--) {
+            // star
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            // space
+            for (int j = 1; j <= 2 * (num - i); j++) {
+                System.out.print("   ");
+            }
+            // star
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        floyds(5);
+        butterfly(4);
     }
 }
