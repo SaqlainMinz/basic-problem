c8118f2cc9a13178732103d934969dd9666bcab1
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-07-08 13:16:22 +0530

    new update

--- using java/patternPart2/pattern.java
+++ using java/patternPart2/pattern.java
@@ -106,10 +106,26 @@ public class pattern {
             System.out.println();
         }
     }
+    //solid rhombus
+    public static void rhombus(int n){
+        //outer loop
+        for(int i=1; i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         // butterfly(5);
         // floyds(5);
-        traingle(5);
+        //traingle(5);
+        rhombus(5);
     }
 }
