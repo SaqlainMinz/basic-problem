b53969618f075c9fc0ef0593021d7f155835da75
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-07-10 21:57:05 +0530

    10 july 2024

--- using java/Array/linearSearch.java
+++ using java/Array/linearSearch.java
@@ -1,14 +1,33 @@
 public class linearSearch {
     public static void main(String[] args) {
-        int number[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
-        int index=LinearSearch(number, 13);
-        if(index==-1){
+        // int number[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+        // int index = LinearSearch(number, 13);
+        // if (index == -1) {
+        //     System.out.println("Not Found");
+        // } else {
+        //     System.out.println("The index of the key is: " + index);
+        // }
+        String menu[] = { "Rawa", "Idli", "Dosa", "Uttapam", "Chilla", "Wada" };
+        int menuIndex=llinearSearch(menu, "Dosa");
+        if(menuIndex==-1){
             System.out.println("Not Found");
         }
         else{
-            System.out.println("The index of the key is: "+index);
+            System.out.println("The Index Of the menu is: "+menuIndex);
         }
 
+
+    }
+
+    public static int llinearSearch(String menu[], String key) {
+        for (int i = 0; i < menu.length; i++) {
+
+            if (menu[i] == key) {
+                return i;
+
+            }
+        }
+        return -1;
     }
 
     public static int LinearSearch(int num[], int key) {
