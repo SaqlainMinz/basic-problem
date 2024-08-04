e655fc164ad94b1685975d8474ec65dac12189a5
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-08-04 23:43:29 +0530

    04 aug 2024

--- using java/sorting/insrtnSort.java
+++ using java/sorting/insrtnSort.java
@@ -1,16 +1,19 @@
+
+import java.util.*;
+
 public class insrtnSort {
     public static void insertionSort(int arr[]) {
-        for(int i=1;i<arr.length;i++){
-            int curr=arr[i];
-            int prev =i-1;
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
 
-            //finding the correct position to enter
-                while(prev>=0 && arr[prev]>curr){
-                    arr[prev+1]=arr[prev];
-                    prev--;
-                }
-                //insertion
-                arr[prev+1]=curr;
+            // finding the correct position to enter
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // insertion
+            arr[prev + 1] = curr;
 
         }
     }
@@ -19,11 +22,13 @@ public class insrtnSort {
         for (int i = 0; i < num.length; i++) {
             System.out.println(num[i]);
         }
-    } 
+    }
 
     public static void main(String[] args) {
         int num[] = { 5, 4, 6, 8, 7, 9, 2, 3, 1 };
-        insertionSort(num);
+        // insertionSort(num);
+        Arrays.sort(num);// inbuilt sorting function
+    
         printSort(num);
     }
 
