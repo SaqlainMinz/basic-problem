25f797c979a04f059b7bef1f8933dd233685a23a
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-08-06 00:04:23 +0530

    06 aug 2024

--- using java/sorting/insrtnSort.java
+++ using java/sorting/insrtnSort.java
@@ -18,16 +18,17 @@ public class insrtnSort {
         }
     }
 
-    public static void printSort(int[] num) {
-        for (int i = 0; i < num.length; i++) {
+    public static void printSort( Integer[] num) {
+        for ( Integer i = 0; i < num.length; i++) {
             System.out.println(num[i]);
         }
     }
 
     public static void main(String[] args) {
-        int num[] = { 5, 4, 6, 8, 7, 9, 2, 3, 1 };
+        Integer num[] = { 5, 4, 6, 8, 7, 9, 2, 3, 1 };
         // insertionSort(num);
-        Arrays.sort(num);// inbuilt sorting function
+        // Arrays.sort(num);// inbuilt sorting function
+        Arrays.sort(num,Collections.reverseOrder());
     
         printSort(num);
     }
